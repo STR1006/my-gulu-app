355964f1bbe703d6cbe0fa92ebc2d25ec2b5471f
BuildTools <unconfigured@null.spigotmc.org>
2025-07-25 21:41:24 -0400

    Correct Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // The 'base' property is essential for GitHub Pages.
+  // It must match your repository name exactly.
   base: "/my-gulu-app/",
 })
